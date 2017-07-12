b11c55bfba7f0788cc64750254b2571fc8ec484d
Ivan Markin <twim@riseup.net>
2017-07-12 15:24:20 +0000

    Add waivers

--- cmd/tlspin-gen/gen.go
+++ cmd/tlspin-gen/gen.go
@@ -1,3 +1,10 @@
+// gen.go - generate keypair for tlspin.
+//
+// To the extent possible under law, Ivan Markin has waived all copyright
+// and related or neighboring rights to tlspin, using the Creative
+// Commons "CC0" public domain dedication. See LICENSE or
+// <http://creativecommons.org/publicdomain/zero/1.0/> for full details.
+
 package main
 
 import (
--- cmd/tlspin-probe/probe.go
+++ cmd/tlspin-probe/probe.go
@@ -1,3 +1,10 @@
+// probe.go - probe TLS public key in tlspin format.
+//
+// To the extent possible under law, Ivan Markin has waived all copyright
+// and related or neighboring rights to tlspin, using the Creative
+// Commons "CC0" public domain dedication. See LICENSE or
+// <http://creativecommons.org/publicdomain/zero/1.0/> for full details.
+
 package main
 
 import (
