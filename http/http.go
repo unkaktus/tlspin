adb4795de57e93295c03186f659c4ec983f8142a
Ivan Markin <twim@riseup.net>
2017-07-16 22:42:00 +0000

    Fix HTTP/2 in http package (#2)

--- http/http.go
+++ http/http.go
@@ -1,22 +1,35 @@
 package tlspinhttp
 
 import (
-	"net"
+	"crypto/tls"
 	"net/http"
 
 	"github.com/nogoegst/tlspin"
+	"golang.org/x/net/http2"
 )
 
-func NewTransport(pubkey string) http.RoundTripper {
+func NewTransport(pubkey string) (http.RoundTripper, error) {
+	var err error
 	t := http.DefaultTransport.(*http.Transport)
-	t.DialTLS = func(network, addr string) (net.Conn, error) {
-		return tlspin.Dial(network, addr, pubkey)
+	t.TLSClientConfig, err = tlspin.TLSClientConfig(pubkey)
+	if err != nil {
+		return nil, err
 	}
-	return t
+	err = http2.ConfigureTransport(t)
+	if err != nil {
+		return nil, err
+	}
+	t.TLSClientConfig.InsecureSkipVerify = true
+	return t, nil
 }
 
 func ListenAndServe(addr, privatekey string, handler http.Handler) error {
-	l, err := tlspin.Listen("tcp", addr, privatekey)
+	tlsConfig, err := tlspin.TLSServerConfig(privatekey)
+	if err != nil {
+		return err
+	}
+	tlsConfig.NextProtos = []string{"h2"}
+	l, err := tls.Listen("tcp", addr, tlsConfig)
 	if err != nil {
 		return err
 	}
