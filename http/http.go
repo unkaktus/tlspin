8094a7b944e7abe6963240b1df30401d7c1d9900
Ivan Markin <twim@riseup.net>
2017-07-19 12:03:26 +0000

    Fix waiver for http package

--- http/http.go
+++ http/http.go
@@ -1,3 +1,10 @@
+// http.go - tlspin supplementary for HTTP.
+//
+// To the extent possible under law, Ivan Markin has waived all copyright
+// and related or neighboring rights to tlspin, using the Creative
+// Commons "CC0" public domain dedication. See LICENSE or
+// <http://creativecommons.org/publicdomain/zero/1.0/> for full details.
+
 package tlspinhttp
 
 import (
