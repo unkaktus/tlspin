a4266fab0deae971ed1f48aa491314a0a2a3a962
Ivan Markin <twim@riseup.net>
2017-07-21 10:53:38 +0000

    Add option for writing key/cert to files

--- cmd/tlspin-gen/gen.go
+++ cmd/tlspin-gen/gen.go
@@ -8,13 +8,18 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"io/ioutil"
 	"log"
 
 	"github.com/nogoegst/tlspin/util"
 )
 
 func main() {
+	var filesFlag = flag.Bool("to-files", false, "Write key and cert into files")
+	flag.Parse()
+
 	sk, err := tlspinutil.GeneratePrivateKey()
 	if err != nil {
 		log.Fatal(err)
@@ -25,4 +30,16 @@ func main() {
 	}
 	fmt.Printf("private key: %v\n", sk)
 	fmt.Printf("public  key: %v\n", pk)
+	cert, privkey, err := tlspinutil.GeneratePEMKeypair(sk)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if *filesFlag {
+		if err := ioutil.WriteFile("key.pem", privkey, 0644); err != nil {
+			log.Fatal(err)
+		}
+		if err := ioutil.WriteFile("cert.pem", cert, 0644); err != nil {
+			log.Fatal(err)
+		}
+	}
 }
